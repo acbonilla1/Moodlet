c3c55d829c821dc9e4753bc24a6965f684a5721e
Andrew Bonilla <acbonilla1@cps.edu>
2021-07-22 12:55:53 -0500

    Added Quote Struct & Randomizer
    This was the file for Quote, and the code in viewcontroller to randomize a quote
    
    and soem buttons for testing

---
+++ banana3Git/Quotes.swift
@@ -0,0 +1,24 @@
+//
+//  Quotes.swift
+//  moodlet project
+//
+//  Created by ENGR-ADMIN on 7/22/21.
+//
+
+import Foundation
+
+struct QuoteObject {
+    var quoteEntry: String
+    var authorName: String
+}
+
+
+var QuoteList = [
+    QuoteObject(quoteEntry: "This is a quote.", authorName: "Me"),
+    QuoteObject(quoteEntry: "This is a quote as well.", authorName: "Me (again)"),
+    QuoteObject(quoteEntry: "Quote quote quote", authorName: "Me (again)"),
+    QuoteObject(quoteEntry: "this is a quote for the fourth time", authorName: "barack obama"),
+    
+]
+
+
--- banana3Git/ViewController.swift
+++ banana3Git/ViewController.swift
@@ -13,8 +13,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("🍌")
+
+        
+        
+    
+        
+        
     }
 
-
+    @IBAction func onSadButtonPressed(_ sender: UIButton) {
+        
+        let randomQuote = SadQuoteList.randomElement()
+        let quoteText = randomQuote?.quoteEntry
+        let quoteAuthor = randomQuote?.authorName
+        
+        print(quoteText?.description, quoteAuthor?.description)
+        
+//        for quote in QuoteList{
+//            let quoteText = quote.quoteEntry
+//            let authorName = quote.authorName
+//            print(quoteText, authorName)
+//        }
+        
+    }
+    
+    @IBAction func onAngryButtonPressed(_ sender: UIButton) {
+        let randomQuote = AngryQuoteList.randomElement()
+        let quoteText = randomQuote?.quoteEntry
+        let quoteAuthor = randomQuote?.authorName
+        
+        print(quoteText?.description, quoteAuthor?.description)
+        
+//        for quote in QuoteList{
+//            let quoteText = quote.quoteEntry
+//            let authorName = quote.authorName
+//            print(quoteText, authorName)
+//        }
+    }
 }
 
