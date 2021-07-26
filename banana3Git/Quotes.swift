517cebc2634eda7a7f3e071629f9c7aafa04266e
Andrew Bonilla <acbonilla1@cps.edu>
2021-07-26 09:11:12 -0500

    added quotes to file
    yea

--- banana3Git/Quotes.swift
+++ banana3Git/Quotes.swift
@@ -21,13 +21,22 @@ var QuoteList = [
 ]
 
 var SadQuoteList = [
-    QuoteObject(quoteEntry: "This is a sad quote.", authorName: "Me"),
+    QuoteObject(quoteEntry: "Some days are just bad days, that's all. You have to experience sadness to know happiness, and I remind myself that not every day is going to be a good day, that's just the way it is!", authorName: "Dita Von Teese"),
+    QuoteObject(quoteEntry: "Heavy hearts, like heavy clouds in the sky, are best relieved by the letting of a little water.", authorName: "Christopher Morley"),
+    QuoteObject(quoteEntry: "First, accept sadness. Realize that without losing, winning isn't so great.", authorName: "Alyssa Milano"),
+    QuoteObject(quoteEntry: "Worry never robs tomorrow of its sorrow, it only saps today of its joy.", authorName: "Leo Buscaglia"),
+    
 ]
 
 var AngryQuoteList = [
-    QuoteObject(quoteEntry: "This is an angry quote.", authorName: "Me"),
+    QuoteObject(quoteEntry:  "In times of great stress or adversity, it's always best to keep busy, to plow your anger and your energy into something positive.", authorName: "Lee Lacocca"),
+    QuoteObject(quoteEntry: "Anger is an acid that can do more harm to the vessel in which it is stored than to anything on which it is poured.", authorName: "Mark Twain"),
+    QuoteObject(quoteEntry: "The greatest remedy for anger is delay.", authorName: "Lucius Annaeus Seneca"),
+    QuoteObject(quoteEntry: "To be angry is to revenge the faults of others on ourselves.", authorName: "Alexander Pope")
 ]
 
 var StressedQuoteList = [
-    QuoteObject(quoteEntry: "This is a quote.", authorName: "Me"),
+    QuoteObject(quoteEntry: "Everyone has the ability to increase resilience to stress. It requires hard work and dedication, but over time, you can equip yourself to handle whatever life throws your way without adverse effects to your health. Training your brain to manage stress won't just affect the quality of your life, but perhaps even the length of it.", authorName: "Amy Morin"),
+    QuoteObject(quoteEntry: "If you are lucky enough to never experience any sort of adversity, we won't know how resilient you are. It's only when you're faced with obstacles, stress, and other environmental threats that resilience, or the lack of it, emerges: Do you succumb or do you surmount?", authorName: "Maria Konnikova"),
+    QuoteObject(quoteEntry: "You should never view your challenges as a disadvantage. Instead, it's important for you to understand that your experience facing and overcoming adversity is actually one of your biggest advantages.", authorName: "Michelle Obama")
 ]
